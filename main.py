0263e139f55294805814e47acd9c0d16417d9c81
Vincent Gagnon <vincentgagnon53@gmail.com>
2020-10-15 10:04:22 -0400

    added some count by functions

--- main.py
+++ main.py
@@ -3,15 +3,49 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
+def time_count(data):
+    by_time = data.groupby(data.Time).size()
+    by_time.index = pd.to_timedelta(by_time.index.astype(str))
+    by_time = by_time.resample('1H').sum()
+
+    by_time.plot(kind='bar')
+    plt.show()
+
+
+def day_of_week_count(data):
+    by_day_of_week = data.groupby(data.DayOfWeek).size()
+    by_day_of_week.plot(kind='bar')
+    plt.show()
+
+
+def category_count(data):
+    by_category = data.groupby(data.Category).size()
+    by_category.plot(kind='bar')
+    plt.yscale('log')
+    plt.show()
+
+
+def by_month_count(data):
+    by_date = data.groupby(data.Date).size()
+    by_date.index = pd.to_datetime(by_date.index)
+    by_date = by_date.resample('1M').sum()
+    by_date.index = by_date.index.month_name()
+    by_date.plot(kind='bar')
+    plt.show()
+
+
+def by_district_count(data):
+    by_district = data.groupby(data.PdDistrict).size()
+    by_district.plot(kind='bar')
+    plt.show()
+
+
 data = pd.read_csv('Police_Department_Incidents_-_Previous_Year__2016_.csv')
-
 data['Time'] = pd.to_datetime(data['Time'], format='%H:%M').dt.time
-
 print(data.info())
-print(data['Category'].unique())
 
-by_time = data.groupby(data.index.Time).mean()
-hourly_ticks = 4 * 60 * 60 * np.arrange(6)
-by_time.plot(xticks=hourly_ticks)
-plt.show()
+
+
+
 
