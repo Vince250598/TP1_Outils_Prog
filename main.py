767cfcdc0d68c37469e77c62191ecbcbb847b48b
Vincent Gagnon <vincentgagnon53@gmail.com>
2020-10-15 11:20:52 -0400

    added crimes category by time period

--- main.py
+++ main.py
@@ -41,11 +41,23 @@ def by_district_count(data):
     plt.show()
 
 
+def crimes_by_time_period(data):
+    top10 = data.Category.value_counts()[:10].index.tolist()
+    data_top10_categories = data[data['Category'].isin(top10)]
+    print(data_top10_categories.Category.unique())
+
+    by_time_crime = data_top10_categories.groupby(['Time', 'Category'])
+    agg_counts = by_time_crime.size().unstack()
+    agg_counts.index = pd.to_timedelta(agg_counts.index.astype(str))
+    agg_counts = agg_counts.resample('4H').sum()
+
+    agg_counts = agg_counts.div(agg_counts.sum(axis=1), axis=0)
+
+    agg_counts.plot(kind='bar')
+    plt.legend(bbox_to_anchor=(0, 1), loc='upper left', ncol=3)
+    plt.show()
+
+
 data = pd.read_csv('Police_Department_Incidents_-_Previous_Year__2016_.csv')
 data['Time'] = pd.to_datetime(data['Time'], format='%H:%M').dt.time
 print(data.info())
-
-
-
-
-
