a3c2f2d98465ccd93d2bec109a4ada74cb95a435
Vincent Gagnon <vincentgagnon53@gmail.com>
2020-10-25 09:58:16 -0400

    Added crime location heatmap

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
-import scipy.stats as stats
+import numpy as np
 
 
 def time_count(data):
@@ -16,12 +14,14 @@ def time_count(data):
 
 def day_of_week_count(data):
     by_day_of_week = data.groupby(data.DayOfWeek).size()
+    by_day_of_week = by_day_of_week.sort_values(ascending=False)
     by_day_of_week.plot(kind='bar')
     plt.show()
 
 
 def category_count(data):
     by_category = data.groupby(data.Category).size()
+    by_category = by_category.sort_values(ascending=False)
     by_category.plot(kind='bar')
     plt.yscale('log')
     plt.show()
@@ -32,12 +32,14 @@ def by_month_count(data):
     by_date.index = pd.to_datetime(by_date.index)
     by_date = by_date.resample('1M').sum()
     by_date.index = by_date.index.month_name()
+    by_date = by_date.sort_values(ascending=False)
     by_date.plot(kind='bar')
     plt.show()
 
 
 def by_district_count(data):
     by_district = data.groupby(data.PdDistrict).size()
+    by_district = by_district.sort_values(ascending=False)
     by_district.plot(kind='bar')
     plt.show()
 
@@ -58,21 +60,21 @@ def crimes_by_time_period(data):
     plt.legend(bbox_to_anchor=(0, 1), loc='upper left', ncol=3)
     plt.show()
 
+def crime_location_heatmap(data):
+    data = data.groupby(['Y', 'X']).size()
+
+    heatmap, xedges, yedges = np.histogram2d(data.index.get_level_values('Y'), data.index.get_level_values('X'),
+                                             bins=100)
+    extent = [data.index.get_level_values('Y')[0], data.index.get_level_values('Y')[-1],
+              data.index.get_level_values('X')[0], data.index.get_level_values('X')[-1]]
+
+    plt.imshow(heatmap, extent=extent)
+    plt.gca().invert_yaxis()
+    plt.show()
+
 
 data = pd.read_csv('Police_Department_Incidents_-_Previous_Year__2016_.csv')
 data['Time'] = pd.to_datetime(data['Time'], format='%H:%M').dt.time
 data['Date'] = pd.to_datetime(data.Date)
 print(data.info())
 
-
-PdDistrict_mapping = {"BAYVIEW": 0, "CENTRAL": 1, "INGLESIDE": 2, "MISSION": 3, "NORTHERN": 4, "PARK": 5, "RICHMOND": 6,
-                      "SOUTHERN": 7, "TARAVAL": 8, "TENDERLOIN": 9}
-
-Y_PdDistrict = data[['PdDistrict', 'Y']].copy()
-Y_PdDistrict.PdDistrict = Y_PdDistrict.PdDistrict.map(PdDistrict_mapping)
-Y_PdDistrict = np.array(Y_PdDistrict)
-print(Y_PdDistrict)
-
-
-
-print(stats.chi2_contingency(Y_PdDistrict))
